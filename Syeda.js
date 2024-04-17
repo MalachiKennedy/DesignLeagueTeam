542d28a21489d154359fa4c4c9edbd6dad80bb2e
Syeda I <144715259+SyedaI7@users.noreply.github.com>
2024-04-17 12:11:35 -0400

    Update Syeda.js

--- Syeda.js
+++ Syeda.js
@@ -2,6 +2,10 @@
 window.addEventListener('load', () => {
   const loader = document.querySelector('.loader');
   setTimeout(() => {
-    loader.setAttribute ('style', 'opacity
-  }
-}
+    loader.setAttribute ('style', 'opacity: 0;');
+  }, 500);
+  setTimeout(() => {
+    loader.parentNode.removeChild(loader);
+    document.body.style.overflow = 'initial';
+  }, 1000);
+});
