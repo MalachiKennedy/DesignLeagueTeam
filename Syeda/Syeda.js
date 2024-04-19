c0aa8ef03e4574c2b5f627735c7c18dc74a718a8
Syeda I <144715259+SyedaI7@users.noreply.github.com>
2024-04-19 12:51:22 -0400

    Update Syeda.js

--- Syeda/Syeda.js
+++ Syeda/Syeda.js
@@ -9,3 +9,92 @@ window.addEventListener('load', () => {
     document.body.style.overflow = 'initial';
   }, 1000);
 });
+
+const coinSfx = new Sfx('sfx/Mario Coin.wav'); 
+const pipeSfx = new Sfx('sfx/Mario Pipe.wav');
+const powerUpSfx = new Sfx('sfx/Mario Powerup.wav')
+
+function Sfx(src) {
+  this.sound = doucment.createElement('audio');
+  this.sound.src = src;
+  this.sound.setAttribute('preload', 'auto');
+  this.sound.setAttribute('controls', 'none');
+  this.sound.style.display = 'none';
+  document-body.appendChild((this.sound);
+  this.play = function() {
+    this.sound.play();
+  };
+}
+
+function Ninjas(name, age, height, color) {
+  this.name = name;
+  this.age = age; 
+  this.height = height; 
+  this.color = color;
+
+  this.coin = () => {
+    //audio
+    coinSfx.play();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
