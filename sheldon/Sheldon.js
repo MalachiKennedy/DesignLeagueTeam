938cd476ec7616860ee5396df8f5280f57da753a
Sheldon George <145591599+sheldontheking@users.noreply.github.com>
2024-05-07 12:08:09 -0400

    Update Sheldon.js

--- sheldon/Sheldon.js
+++ sheldon/Sheldon.js
@@ -43,4 +43,52 @@ const button = document.querySelector('#btnCoin');
     button.style.setProperty('color','var(--bg-color)');
 
     //coin effect
-    const coinImg = document.create
+    const coinImg = document.createElement('img');
+    coinImg.setAttribute('src','imgs/coin.png');
+    coinImg.setAttribute('id','coin-img');
+    coinImg.style.transform = 'translate(-50%,calc(-100% - ' + ninja.height + 'px)';
+    container.appendChild(coinImg);
+
+    $('#coin-img').animate({top: '60%', opacity: 1 }, 250);
+    $('Ecoin-img').animate({ top: '70', opacity: 0 }, 250, () => {
+      $('#coin-img').remove();
+      button.style.setProperty('background-color', 'transparent');
+      button.style.setProperty('border-color', 'var(--accent-color');
+      button.style.setProperty('color', 'var(--accent-color');
+    });
+  };
+  this.pipe =() => {
+    pipeSfx.play();
+  };
+  this.powerUp = () => {
+    pipeSfx.play();
+  };
+  this.powerUp = () => {
+    powerUpSfx.play();
+  };
+}
+const promptUser = () => {
+  let name = prompt('Pick a name:') || 'Ninja';
+  let age;
+  let height;
+  let color;
+
+  this.getAge = () => {
+    age = prompt('Pick an age:') || '18';
+    age = parseInt(age);
+    if (isNaN(age)) {
+      this.getAge();
+    } else {
+      return;
+    }
+  };
+  this.getHeight = () => {
+    height= prompt('Pick height: 1, 2, 3, 4') || 4;
+    height = parseInt(height);
+    if (height === 1 || height == 2 || height === 3 || height === 4) {
+      height = height * 64;
+      return;
+    } else {
+      this.getHeight();
+    }
+  };
